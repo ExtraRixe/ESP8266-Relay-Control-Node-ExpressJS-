f350b3275a13f69cc3017a5294c9678ad7a82c1e
Kyle Concepcion <45564653+ExtraRixe@users.noreply.github.com>
2021-12-16 02:41:39 +0000

    Added delete button for each time data Added reset button
    Added comments

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ var data = // 0 = on, 1 = off
 
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //if '/' load index.html
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/node', (req, res) => res.json(data));
+app.get('/node', (req, res) => res.json(data));  //if 'node' load JSON data
 
-app.get('/status', (req, res) => {
+app.get('/status', (req, res) => {  //if 'status' load object status only, this is what the arduino reads for data.
   var status_arr = JSON.parse(JSON.stringify(data))
 
   for (x in status_arr){
@@ -30,7 +30,7 @@ app.get('/status', (req, res) => {
   res.json(status_arr)
 });
 
-app.get('/switch', function(req, res) {
+app.get('/switch', function(req, res) { // if server recieves GET 'switch' toggle specific relay status
   const relay = req.query.relay;
   const status = parseInt(req.query.status);
 
@@ -41,7 +41,7 @@ app.get('/switch', function(req, res) {
   res.send('recieved')
 });
 
-app.get('/timer', function(req, res) {
+app.get('/timer', function(req, res) { // if server recieves GET 'time' set timer to JSON
   const relay = req.query.relay;
   const start = req.query.start;
   const end = req.query.end;
@@ -55,11 +55,32 @@ app.get('/timer', function(req, res) {
   res.redirect('/');
 });
 
+app.get('/delete', function(req, res) { //if server recieves GET 'delete' delete specific time for the specific relay
+  const relay = req.query.relay;
+  const index = req.query.time;
+
+  data[relay].start.splice(index, 1)
+  data[relay].end.splice(index, 1)
+
+  console.log(data[relay])
+  res.redirect('/');
+});
+
+app.get('/reset', function(req, res) { //if server recieves GET 'reset' clears all relay status and time
+  for (x in data){
+    data[x].start = []
+    data[x].end = []
+    data[x].status = 1
+  }
+
+  res.redirect('/');
+});
+
 app.listen(3000, () => {
   console.log("Started on PORT 3000");
 })
 
-setInterval(function() {
+setInterval(function() { //code repeats ever 1s (1000ms), Checks the start and end time for each relay, This is the part where it automatically toggles the relays ON or OFF if time is set on the relays.
 
   let options = {
     timeZone: 'Asia/Manila',
@@ -70,24 +91,22 @@ setInterval(function() {
   },
   formatter = new Intl.DateTimeFormat([], options);
 
-  var cur = formatter.format(new Date())
+  var cur = formatter.format(new Date()) //current time
 
   for (const x in data) { //cycle each relay
 
-    if (data[x].start.length > 0) {
+    if (data[x].start.length > 0) { // if there is time set in the relay
 
-      let start = data[x].start[0]
-      let end = data[x].end[0]
+      let start = data[x].start[0] //gets the time in array start index 0 (first in line)
+      let end = data[x].end[0] //gets the time in array end index 0 (first in line)
 
-      //console.log(`cur:${cur}\nstart: ${start}\nend:${end}`)
-
-      if (cur >= start && cur <= end) {
-        data[x].status = 0
+      if (cur >= start && cur <= end) { // If current time is greater than the start time in the array AND current time is less than the end time in the array
+        data[x].status = 0 //turn on 
       } else {
-        data[x].status = 1
+        data[x].status = 1 //else, turn it off
       }
 
-      if (cur > end) {
+      if (cur > end) { // if current time is greater than end, move start and end time in index 0 to the end of the array.
         data[x].start.push(data[x].start[0])
         data[x].end.push(data[x].end[0])
         data[x].start.shift()
